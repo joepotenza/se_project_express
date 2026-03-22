0a035d6bffd1e59cff22567428e8fda6a592a769
Joe Potenza <joseph.potenza@gmail.com>
2026-03-22 04:55:13 -0400

    Remove unused call to auth

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const auth = require("./middlewares/auth");
 
 const app = express();
 const { PORT = 3001 } = process.env;
